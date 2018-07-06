5c7fd7cc2eeca21c0ad06e6f9ac4dff6cf341b4c
anshuman2010 <40779839+anshuman2010@users.noreply.github.com>
2018-07-06 17:29:42 +0530

    new

--- Hello.java
+++ Hello.java
@@ -1,5 +1,5 @@
-class HelloWorl {
-public.static.void.main(string args[]){
-system.out.println("hello anshu");
-}
+class Hello{  
+    public static void main(String args[]){  
+     System.out.println("Hello Java");  
+    }  
 }
